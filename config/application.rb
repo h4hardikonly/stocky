0463f701433ba895e3247d1972f4eb2cc2f6f097
h4hardikonly <h4hardikonly@gmail.com>
2017-10-29 01:53:01 +0530

    1. converted application.html from erb to haml 2. translation support 3. designed homepage

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,8 @@ module Stocky
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '{*.*.,*.}{rb,yml}')].sort
+    config.i18n.default_locale = :en #for now TO DO, change this?
   end
 end
